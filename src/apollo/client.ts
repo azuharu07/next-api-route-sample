302ed9124ea8ce46db71985f8cde0b3d894ca311
azuharu07 <azuha07i@gmail.com>
2022-07-12 00:42:23 +0900

    :pencil2: Use env

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL,
   cache: new InMemoryCache()
 })
